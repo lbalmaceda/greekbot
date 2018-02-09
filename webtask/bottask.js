b23432c3564eba270b62399e3ee28603efe61963
Luciano Balmaceda <balmacedaluciano@gmail.com>
2018-02-09 20:02:49 +0200

    working authentication and event capture

--- webtask/bottask.js
+++ webtask/bottask.js
@@ -1,48 +1,53 @@
 'use latest';
-require('dotenv').config()
 
 import express from 'express';
 import { fromExpress } from 'webtask-tools';
 import bodyParser from 'body-parser';
 const app = express();
 
+app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
+app.post('/slack/events', (req, res) => {
+  console.log("body:");
   console.log(req.body);
 
   if (req.body.type === 'url_verification'){
-    //Slack verification handshake
+    //Slack verification handshake. Move this to a middleware
     console.log("Handling Slack handshake");
     var data = {};
     data.slack_verification_token = req.body.token;
-    return req.webtaskContext.storage.set(data, { force: 1 }, (error) => {
+    req.webtaskContext.storage.set(data, { force: 1 }, (error) => {
       if (error) {
+        console.log("wt storage error:");
         console.log(error);
-        return res.status(500).send("Handshake failed");
+        return res.status(500)
+                .send("Handshake failed");
       } 
       return res.status(200)
         .send({"challenge" : req.body.challenge});
     });
+    return;
   }
 
   if (req.body.type === 'event_callback'){
     console.log("New event received!");
     req.webtaskContext.storage.get((error, data) => {
-      if (error || data.slack_verification_token !== data.slack_verification_token){
-        console.log("Request doesn't seems to come from Slack. Error: " + error);
-        //Ignore gracefully
+      if (error || !req.body.token || req.body.token !== data.slack_verification_token){
+        console.log("Request doesn't seems to come from Slack. Error:");
+        console.log(error);
         return res.sendStatus(200);
       }
-      return handleEvent(req.body.event);
+      handleEvent(req.body.event.type, req.body);
+      return res.sendStatus(200);
     });
+    return;
   }
   
   return res.status(500)
            .send("Can't handle this body");
 });
 
-var handleEvent = (event) => {
-
+var handleEvent = (type, event) => {
 
 }
 
